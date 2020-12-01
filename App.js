a9b9128e775cbd4e2bd128effeff2198376438b3
Walisson Rodrigues <contact@walisson.com>
2020-12-01 20:33:14 +1100

    App Intro Slider implementation

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const slides = [
+  {
+    key: 'one',
+    title: 'JUST TRAVEL',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    image: require("./images/1.png")
+  },
+  {
+    key: 'two',
+    title: 'TAKE A BREAK',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    image: require("./images/2.png")
+  },
+  {
+    key: 'three',
+    title: 'ENJOY YOUR JOURNEY',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    image: require("./images/3.png")
+  }
+]
+
+export default class App extends Component {
+
+  state = { showHomePage: false }
+
+  _renderItem = ({ item }) => {
+    return (
+      <View style={{ flex: 1 }}>
+        <Image
+          source={item.image}
+          style={{
+            resizeMode: 'cover',
+            height: '73%',
+            width: '100%'
+          }}
+        />
+      </View>
+    )
+  }
+
+  render() {
+    return (
+      <AppIntroSlider
+        renderItem={this._renderItem}
+        data={slides}
+      />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
